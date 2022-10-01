1a4f8282ddbab1e6f6fd4ef9f89d89351316b389
skynesser <3250532948@qq.com>
2022-10-01 20:58:07 +0800

    阶段性完结

--- src/main/java/com/jking412/tagle/tagleenum/Message.java
+++ src/main/java/com/jking412/tagle/tagleenum/Message.java
@@ -5,19 +5,26 @@ import lombok.Getter;
 @Getter
 public enum Message {
 
-    inputOrderMsg("输入值是选项前的序号","msg"),
-    noContentMsg("当前条目没有内容，请先去添加内容","tip"),
-    successMsg("操作成功","tip"),
-    failMsg("操作失败","tip"),
-    inputIntError("输入值应该是一个整数","error"),
-    outRangeError("输入值不在合法范围内","error"),
-    inputParseError("你的输入无法被解析","error");
+    //31 红色
+    //32 黄色
+    //36 绿色
+
+    inputOrderMsg("输入值是选项前的序号","msg",31),
+    noContentMsg("当前条目没有内容，请先去添加内容","tip",32),
+    successMsg("操作成功","tip",36),
+    failMsg("操作失败","tip",32),
+    inputIntError("输入值应该是一个整数","error",31),
+    outRangeError("输入值不在合法范围内","error",31),
+    inputParseError("你的输入无法被解析","error",31);
 
     private String message;
     private String type;
 
-    Message(String message, String type){
+    private int color;
+
+    Message(String message, String type,int color){
         this.message = message;
         this.type = type;
+        this.color = color;
     }
 }
--- src/main/java/com/jking412/tagle/utils/MenuUtils.java
+++ src/main/java/com/jking412/tagle/utils/MenuUtils.java
@@ -3,16 +3,24 @@ package com.jking412.tagle.utils;
 import com.jking412.tagle.tagleenum.Message;
 
 public class MenuUtils {
+
+    /**
+     * @param colour  颜色代号：背景颜色代号(41-46)；前景色代号(31-36)
+     * @param content 要打印的内容
+     */
+    private static String getFormatLogString(String content, int colour) {
+        return String.format("\033[%dm%s\033[0m", colour, content);
+    }
     public static void outputMsg(Message message){
         String type = message.getType();
         if(type.equals("msg")){
             System.out.println("请你输入一个值");
-            System.out.println(message.getMessage());
+            System.out.println(getFormatLogString(message.getMessage(), message.getColor()));
         }else if(type.equals("error")){
             System.out.println("输入错误，请重新输入");
-            System.out.println(message.getMessage());
+            System.out.println(getFormatLogString(message.getMessage(), message.getColor()));
         } else if (type.equals("tip")) {
-            System.out.println(message.getMessage());
+            System.out.println(getFormatLogString(message.getMessage(), message.getColor()));
         }
     }
 }
